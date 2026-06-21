ecc6d31c96bb19df51fa4bb03aebf1cb99ef5c65
Stan238 <28St03R@protonmail.com>
2026-06-21 09:55:34 -0400

    added out put list files and method to delete file

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,11 @@ void main() {
         Scanner sc = new Scanner(System.in);
         System.out.println("""
                 Menu:\s
-                 1. Write to file
-                 2. Read file
-                 3. Exit""");
+                 1. List files
+                 2. Write
+                 3. Read
+                 4. Delete
+                 5. Exit""");
         System.out.print("Your selection:");
         int menuNumber;
         try {
@@ -27,6 +29,11 @@ void main() {
 
         switch (menuNumber) {
             case 1:
+                System.out.println("=== List files===");
+                fileNote.listFiles();
+                System.out.println("=== === === === ===");
+               break;
+            case 2:
                 /* get name file
                  * check file is exist
                  * create file / open file
@@ -51,12 +58,13 @@ void main() {
                      logNote.writeLog("Unsuccessfully attempt wrote file ");
                  }
                 break;
-            case 2:
+            case 3:
                 /* get name file
                  * check file is exist
                  *  open file
                  * read file
                  * close file*/
+//                TODO create methods to check name file and set name File Note
                 isName = true;
                 do {
                     System.out.print("Enter file name: ");
@@ -73,7 +81,24 @@ void main() {
                 fileNote.readNote();
                 logNote.writeLog("Read file");
                 break;
-            case 3:
+            case 4:
+                System.out.println("Do you want to delay file?");
+                isName = true;
+                do {
+                    System.out.print("Enter file name: ");
+                    String nameOfFile;
+                    nameOfFile = sc.next();
+                    if (nameOfFile.indexOf('/') == -1) {
+                        fileNote.setFileNote(nameOfFile);
+                        isName = false;
+                    } else {
+                        System.out.println("Contain unacceptable characteristics");
+                        logNote.writeLog("Contain unacceptable characteristics - " + nameOfFile);
+                    }
+                } while (isName);
+                fileNote.deleteNote();
+                break;
+            case 5:
                 System.out.println("Good buy,\n See you soon!");
                 chooseMenu = false;
                 sc.close();
--- src/Note.java
+++ src/Note.java
@@ -1,4 +1,9 @@
+import java.awt.*;
 import java.io.*;
+import java.net.URI;
+import java.nio.file.*;
+import java.nio.file.spi.FileTypeDetector;
+import java.util.Arrays;
 import java.util.Scanner;
 //import java.io.FileWriter;
 //import java.io.IOException;
@@ -7,14 +12,14 @@ import java.util.Scanner;
 
 public class Note {
     private File fileNote;
-    private final Log logNote= new Log();
+    private final Log logNote = new Log();
 
 //   public File getFileNote() {
 //        return fileNote;
 //    }
 
     public void setFileNote(String filename) {
-        this.fileNote = new File("Notes/"+filename+".txt");
+        this.fileNote = new File("Notes/" + filename + ".txt");
     }
 
     public boolean writeToNote() {
@@ -24,10 +29,10 @@ public class Note {
 //           Create file safely
             Scanner sc = new Scanner(System.in);
             String answerCreate = sc.next();
-            if(answerCreate.equals("yes")){
+            if (answerCreate.equals("yes")) {
                 try {
-                    if(fileNote.createNewFile()){
-                        System.out.printf("Create file %s " , fileNote.getName());
+                    if (fileNote.createNewFile()) {
+                        System.out.printf("Create file %s ", fileNote.getName());
                         logNote.writeLog("Create file");
                     }
                 } catch (IOException e) {
@@ -40,7 +45,7 @@ public class Note {
 
         }
 
-        try (FileWriter writer =new FileWriter(fileNote, true)){
+        try (FileWriter writer = new FileWriter(fileNote, true)) {
 
             PrintWriter writerPrint = new PrintWriter(writer);
             MessageFile message = new MessageFile();
@@ -48,32 +53,53 @@ public class Note {
             writerPrint.println(message.getMassage());
             writerPrint.close();
 
-        } catch( RuntimeException | IOException e) {
+        } catch (RuntimeException | IOException e) {
             logNote.writeLog(e.toString());
         }
         return true;
 
     }
-// Reading from file
+
+    // Reading from file
     public void readNote() {
-      try (BufferedReader inputStream = new BufferedReader(new FileReader(fileNote))){
-          String line;
-          System.out.println("==== Start Note ====");
-          while((line = inputStream.readLine()) != null) {
-              System.out.println(line);
-          }
-          System.out.println("==== End Note ====");
-      } catch (FileNotFoundException e) {
-          System.out.println("File not found");
-          logNote.writeLog(e.toString());
-      } catch (IOException ioException) {
-          logNote.writeLog(ioException.getMessage());
-      } finally {
-          System.out.println("Choose exist file");
-      }
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(fileNote))) {
+            String line;
+            System.out.println("==== Start Note ====");
+            while ((line = inputStream.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("==== End Note ====");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            logNote.writeLog(e.toString());
+        } catch (IOException ioException) {
+            logNote.writeLog(ioException.getMessage());
+        } finally {
+            System.out.println("Choose exist file");
+        }
 
     }
 
+    public void listFiles() {
+       File derictoryNote = new File("Notes");
+
+
+       for(int index=0; index<derictoryNote.listFiles().length; index++ ){
+           System.out.println(derictoryNote.listFiles()[index].toString());
+       }
+    }
+
+    public boolean deleteNote(){
+
+        if(fileNote.delete()) {
+            logNote.writeLog("File "+ fileNote.getName() + "deleted");
+            return true;
+        } else {
+            logNote.writeLog("File "+ fileNote.getName() + "can not be delete!!");
+            return false;
+        }
+
+    }
 
 
 
